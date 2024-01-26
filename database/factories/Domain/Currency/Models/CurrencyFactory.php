d0ab9f497715afa664b2d9ef018db54be53cd469
Petar Vladisavljev <petar.vladisavljev@gmail.com>
2024-01-26 20:52:05 +0100

    Add expanded currency resource controller test

--- database/factories/Domain/Currency/Models/CurrencyFactory.php
+++ database/factories/Domain/Currency/Models/CurrencyFactory.php
@@ -9,7 +9,7 @@ class CurrencyFactory extends Factory
     public function definition(): array
     {
         return [
-            'iso' => $this->faker->currencyCode(),
+            'iso' => $this->faker->unique()->currencyCode(),
             'exchange_rate' => $this->faker->randomFloat(6, 0, 10000),
             'surcharge_percentage' => $this->faker->randomFloat(2, 0, 100),
             'discount_percentage' => $this->faker->randomFloat(2, 0, 100),
--- tests/Feature/Http/Api/Controllers/CurrencyResourceControllerTest.php
+++ tests/Feature/Http/Api/Controllers/CurrencyResourceControllerTest.php
@@ -2,13 +2,32 @@
 
 namespace Tests\Feature\Http\Api\Controllers;
 
+use App\Domain\Currency\Models\Currency;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class CurrencyResourceControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function testIndex()
     {
+        /** @var Currency[] $currencies */
+        $currencies = Currency::factory()->count(3)->create();
         $response = $this->getJson('/api/currencies');
-        $response->assertStatus(200);
+
+        $response->assertStatus(200)
+            ->assertJsonCount(3, 'data');
+        foreach ($currencies as $currency) {
+            $response->assertJsonFragment([
+                [
+                    'id' => $currency->id,
+                    'iso' => $currency->iso,
+                    'exchange_rate' => $currency->exchange_rate,
+                    'surcharge_percentage' => $currency->surcharge_percentage,
+                    'discount_percentage' => $currency->discount_percentage,
+                ],
+            ]);
+        }
     }
 }
